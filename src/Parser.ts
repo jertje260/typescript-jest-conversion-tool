41c8b6824eb9eba72be84d50e01e0ae94421aa6b
Jeroen Broekhuizen <j_w_broekhuizen@hotmail.com>
2019-10-11 22:43:59 +0200

    make parser method public

--- src/Parser.ts
+++ src/Parser.ts
@@ -1,7 +1,7 @@
 import { IPackageJson } from "./models/IPackageJson";
 
 export class Parser {
-	ParsePackageJson(input: string): IPackageJson {
+	public ParsePackageJson(input: string): IPackageJson {
 		return JSON.parse(input);
 	}
 }
